be1c8d4c859670a734630cbab75594647a805723
yavlash-liza <yavlashliza@gmail.com>
2022-06-20 23:12:28 +0300

    adding pojo and interfaces

---
+++ api/src/main/java/com/yavlash/api/core/review/ReviewService.java
@@ -0,0 +1,12 @@
+package com.yavlash.api.core.review;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface ReviewService {
+    @GetMapping(
+            value = "/review",
+            produces = "application/json")
+    List<Review> getReviews(@RequestParam(value = "productId", required = true) int productId);
+}
