0086b2e6ca6caf6750b917b1f2ce0b72a8a08c43
yavlash-liza <yavlashliza@gmail.com>
2022-06-21 16:19:55 +0300

    implementing api: add product service implementation file

--- api/src/main/java/com/yavlash/api/core/product/ProductService.java
+++ api/src/main/java/com/yavlash/api/core/product/ProductService.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 public interface ProductService {
-    @@GetMapping(
+    @GetMapping(
             value = "/product/{productId}",
             produces = "application/json")
     Product getProduct(@PathVariable int productId);
