49425f8ca6bac607134433bd44615470fa841ad5
yavlash-liza <yavlashliza@gmail.com>
2022-06-26 20:06:27 +0300

    change mongodb version

--- microservices/recommendation-service/src/test/java/com/yavlash/microservices/core/recommendation/MongoDbTestBase.java
+++ microservices/recommendation-service/src/test/java/com/yavlash/microservices/core/recommendation/MongoDbTestBase.java
@@ -5,7 +5,7 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 
 public abstract class MongoDbTestBase {
-    private static MongoDBContainer database = new MongoDBContainer("mongo:4.4.2");
+    private static MongoDBContainer database = new MongoDBContainer("mongo:latest");
 
     static {
         database.start();
