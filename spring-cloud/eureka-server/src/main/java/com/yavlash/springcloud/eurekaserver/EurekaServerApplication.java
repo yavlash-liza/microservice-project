92a1ac5b666aafee7bd4a5241e4f30a49a835be0
yavlash-liza <yavlashliza@gmail.com>
2022-07-08 22:51:08 +0300

    create eureka-server module

---
+++ spring-cloud/eureka-server/src/main/java/com/yavlash/springcloud/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,11 @@
+package com.yavlash.springcloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
