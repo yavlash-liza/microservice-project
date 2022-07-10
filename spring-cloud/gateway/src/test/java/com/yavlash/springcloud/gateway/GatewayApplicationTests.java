8634c2cb02031d3216902fd042132bdcb1c978dd
yavlash-liza <yavlashliza@gmail.com>
2022-07-10 18:26:46 +0300

    enable eureka in tests

--- spring-cloud/gateway/src/test/java/com/yavlash/springcloud/gateway/GatewayApplicationTests.java
+++ spring-cloud/gateway/src/test/java/com/yavlash/springcloud/gateway/GatewayApplicationTests.java
@@ -1,13 +1,12 @@
 package com.yavlash.springcloud.gateway;
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT, properties = {"eureka.client.enabled=false"})
 class GatewayApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
